f080e3c283915644ed208200012ec33aef9f90ba
TPpomy <satapon041142@gmail.com>
2021-12-29 11:09:01 +0700

    Update Time : 11.08

--- Myfs.js
+++ Myfs.js
@@ -6,5 +6,16 @@ console.log(data);
 //writeflie
 
 const writeflie = `This is NodeJs \n ${data} \n ${new Date()}`;
-fs.writeFileSync("myflie/datawrite.txt", writeflie);
-console.log(writeflie);
+/*fs.writeFileSync("myflie/datawrite.txt", writeflie);
+console.log(writeflie);*/
+console.log("/////////////////// This is fs Synchronous ///////////////////");
+
+/////////////////////////////////////////fs Synchronous
+
+fs.readFile("myflie/data.txt", "utf8", (err, data) => {
+  if (err) return console.log(err);
+  console.log(data);
+  fs.writeFile("myflie/dataoutput.txt", writeflie, (err) => {
+    if (err) return console.log(err);
+  });
+});
