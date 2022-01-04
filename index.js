df19c1079aa0124bb612f4399a3bf7c4ac8fce34
TPpomy <satapon041142@gmail.com>
2022-01-04 13:47:08 +0700

    Update 04-01-2565

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const http = require("http");
+const fs = require("fs");
 
+const indexpaege = fs.readFileSync(`${__dirname}/webpages/index.html`);
+const productpage = fs.readFileSync(`${__dirname}/webpages/product.html`);
+
+const port = 3000;
 const server = http.createServer(function (req, res) {
-  res.write("Hello");
+  const pathname = req.url;
+  if (pathname === "/" || pathname === "/home") {
+    res.write(indexpaege);
+  }
+  if (pathname === "/product") {
+    res.write(productpage);
+  }
   res.end();
 });
 
-server.listen(3000);
+server.listen(port, "localhost", () => {
+  console.log(`Listening Port ${port}`);
+});
